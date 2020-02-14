d43ff1d53b7088c367f426cb3254fecfaf0c5a5e
eewano <eewano@gmail.com>
2020-02-14 17:17:30 +0900

    サイト全体を管理するcssとjavascriptの土台が完成。

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function myfunc() {
+    let mymenu = document.querySelector(".menu_area");
+    let changeImg = document.querySelector(".img_change");
+
+    if (mymenu.style.maxHeight) {
+        mymenu.style.maxHeight = null;
+        changeImg.setAttribute('src', 'img/menu_icon_open.png');
+    } else {
+        mymenu.style.maxHeight = mymenu.scrollHeight + "px";
+        changeImg.setAttribute('src', 'img/menu_icon_close.png');
+    }
+}
