01e0bbd8c9484d34ceebc93a9180df7791cad22d
eewano <eewano@gmail.com>
2020-02-14 19:27:33 +0900

    フォルダ階層の違いにより、関数を追加。

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ function myfunc() {
         changeImg.setAttribute('src', 'img/menu_icon_close.png');
     }
 }
+
+function myfunc_admin() {
+    let mymenu = document.querySelector(".menu_area");
+    let changeImg = document.querySelector(".img_change");
+
+    if (mymenu.style.maxHeight) {
+        mymenu.style.maxHeight = null;
+        changeImg.setAttribute('src', '../img/menu_icon_open.png');
+    } else {
+        mymenu.style.maxHeight = mymenu.scrollHeight + "px";
+        changeImg.setAttribute('src', '../img/menu_icon_close.png');
+    }
+}
