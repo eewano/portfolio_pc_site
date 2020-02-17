2e203685c15a69ebfd79485440508350f47a6c6a
eewano <eewano@gmail.com>
2020-02-17 16:24:20 +0900

    商品の値段を専用表示にする為、number_format関数を追加。

--- method_php/method.php
+++ method_php/method.php
@@ -4,6 +4,11 @@ function h01($string) {
     return htmlspecialchars($string, ENT_QUOTES, "UTF-8");
 }
 
+function h02($string) {
+    $yen = htmlspecialchars($string, ENT_QUOTES, "UTF-8");
+    return number_format($yen);
+}
+
 function sanitize($before) {
     foreach ($before as $key => $value) {
         $after[$key] = htmlspecialchars($value);
