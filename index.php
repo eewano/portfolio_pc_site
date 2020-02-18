705a2371adaaa47303192893e1b6e1858f5c41d5
eewano <eewano@gmail.com>
2020-02-18 15:38:22 +0900

    トップページphpとcssの土台が完成。

--- index.php
+++ index.php
@@ -5,16 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio PC Site</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="custom_css/index.css">
+    <link rel="stylesheet" href="css/ress.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
 
 <?php
 
-try {
+    require_once('method_php/method.php');
     require_once('method_php/get_user_pass.php');
 
+try {
     $dsn = 'mysql:dbname=portfolio_pc_shop; host=localhost; charset=utf8';
     $user = getDBUser();
     $password = getDBPass();
@@ -25,129 +29,81 @@ try {
     $stmt = $dbh -> prepare($sql);
     $stmt -> execute();
 
+    $row_count = $dbh -> query($sql) -> rowCount();
+
     $dbh = null;
 
 } catch (Exception $e) {
-    header('Location: ../../site_err.php');
+    header('Location: site_err.php');
 	exit();
 }
 
 ?>
 
     <header>
-        <div class="container header_area">
-            <div class="row">
-                <div class="header_logo col-12">
-                    <div class="header_logo_area mx-auto my-4 px-4 py-4 text-center text-dark">
-                        <p class="h1 font-weight-bold">PC SHOP</p>
-                        <p class="h2 font-weight-bold">お値段.co.jp</p>
-                    </div>
-                </div>
-                <div class="header_list_area col-12">
-                    <ul class="list-inline">
-                        <li class="list-inline-item mx-0 text-center">
-                            <a href="" class="nav-link text-dark">トップページ</a>
-                        </li>
-                        <li class="list-inline-item mx-0 text-center">
-                            <a href="admin_login.php" class="nav-link text-dark">管理者画面</a>
-                        </li>
-                        <li class="list-inline-item mx-0 text-center">
-                            <a href="" class="nav-link text-dark">登録ユーザー</a>
-                        </li>
-                        <li class="list-inline-item mx-0 text-center">
-                            <a href="" class="nav-link text-dark">その他</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="header_image_area col-12">
-                    <img src="img/header_image01_01.jpg" class="img-fluid" alt="">
-                </div>
-            </div>
-        </div>
+        <h1 class="header_logo">
+            <a href="#">PC e2wn</a>
+        </h1>
     </header>
 
+    <button type="button" onclick="myfunc()">
+        <img src="img/menu_icon_open.png" class="img_change" alt="">
+    </button>
+
+    <div class="menu_area">
+        <ul>
+            <li><a href="index.php">トップ</a></li>
+            <li><a href="#">ログイン</a></li>
+            <li><a href="#">カート</a></li>
+            <li><a href="admin_login.php">管理者用</a></li>
+        </ul>
+    </div>
+
+    <div class="top_image_area">
+        <img src="img/header_image01_01.jpg" class="top_image" alt="">
+    </div>
+
+    <div class="title_area">
+        <p class="caution">※当Webサイトはeewanoのポートフォリオサイトとなっております。商品の選択から購入までの手続きのシミュレーションを行えますが、実際の購入及びメールの送信等は出来ませんのでご了承下さい。</p>
+        <h2>PCショップ eewano</h2>
+        <?php if ($row_count == 0): ?>
+        <h3>現在、商品は取り扱っておりません。</h3>
+        <?php else: ?>
+        <h3>登録商品一覧</h3>
+        <?php endif; ?>
+    </div>
+
     <main>
-        <div class="container">
-            <div class="row">
-                <div class="warning_area col-12">
-                    <p class="mx-3 my-5 text-danger font-weight-bold">※当Webサイトはeewanoのポートフォリオサイトとなっております。商品の選択から購入までの手続きのシミュレーションを行えますが、実際の購入及びメールの送信等は出来ませんのでご了承下さい。</p">
-                </div>
-
-                <h3 class="col-12 my-5 text-center">商品一覧</h3>
-                <?php while (true) {; ?>
-                <?php $rec = $stmt -> fetch(PDO::FETCH_ASSOC); ?>
-                <?php if ($rec == false) { break; }; ?>
-                    <div class="product_area col-12 col-sm-6 col-lg-4">
-                        <div class="product_area_inner border mb-4 px-3 py-3">
-                            <img src="/img/<?php echo $rec['image']; ?>" class="img-fluid" alt="">
-                            <p class="product_name pt-3"><?php echo $rec['name']; ?></p>
-                            <p class="product_price mb-0 text-danger font-weight-bold">¥<?php echo $rec['price']; ?></p>
-                            <p class="evaluation text-warning"><?php echo $rec['evaluation']; ?></p>
-                            <p class="product_review"><?php echo $rec['detail']; ?></p>
-                        </div>
-                    </div>
-                <?php }; ?>
-
-                <h3 class="col-12 my-5 text-center">当サイトの構成</h3>
-                <div class="constitution_area col-12 col-sm-6">
-                    <div class="constitution_area_inner mb-4 py-5">
-                        <h4 class="text-center mb-5">フロントエンド</h4>
-                        <ul class="pl-6 pr-4">
-                            <li>HTML5 and CSS(SASS)</li>
-                            <li>レスポンシブ対応はBootstrap4を使用</li>
-                            <li>jQuery</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="constitution_area col-12 col-sm-6">
-                    <div class="constitution_area_inner mb-4 py-5">
-                        <h4 class="text-center mb-5">バックエンド</h4>
-                        <ul class="pl-6 pr-4">
-                            <li>PHP（フレームワーク未使用）</li>
-                            <li>データベース管理はMysqlを使用</li>
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
+        <div class="product_area">
+        <?php while (true): ?>
+        <?php $rec = $stmt -> fetch(PDO::FETCH_ASSOC); ?>
+        <?php if ($rec == false): ?>
+        <?php break; ?>
+        <?php endif; ?>
+            <a href="#" class="product_box">
+            <?php if ($rec['image'] == ''): ?>
+                <img src="img/no_image.jpg" alt="" class="product_image">
+            <?php else: ?>
+                <img src="img/<?php echo $rec['image']; ?>" alt="">
+            <?php endif; ?>
+                <p class="product_name"><?php echo h01($rec['name']); ?></p>
+                <p class="product_price">¥ <?php echo h_price($rec['price']); ?></p>
+                <p class="product_evaluation"><?php echo h_evaluation($rec['evaluation']); ?></p>
+                <p class="product_review"><?php echo h01($rec['detail']); ?></p>
+            </a>
+        <?php endwhile; ?>
         </div>
     </main>
 
     <footer>
-        <div class="container bg-warning mt-5">
-            <div class="row py-4">
-                <div class="footer_logo col-12 col-md-4">
-                    <div class="footer_logo_area mb-4 text-center text-dark">
-                        <p class="h3 font-weight-bold">PC SHOP</p>
-                        <p class="h4 font-weight-bold">お値段.co.jp</p>
-                    </div>
-                </div>
-
-                <div class="footer_link col-12 col-md-8">
-                    <ul class="list-inline">
-                        <li class="list-inline-item mx-0">
-                            <a href="" class="nav-link text-dark text-center">トップページ</a>
-                        </li>
-                        <li class="list-inline-item mx-0">
-                            <a href="" class="nav-link text-dark text-center">管理者画面</a>
-                        </li>
-                        <li class="list-inline-item mx-0">
-                            <a href="" class="nav-link text-dark text-center">登録ユーザー</a>
-                        </li>
-                        <li class="list-inline-item mx-0">
-                            <a href="" class="nav-link text-dark text-center">その他</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <p class="text-center col-12 mb-0">©2020 eewano portfolio</p>
-            </div>
+        <div class="footer_layout">
+            <h1>PC e2wn</h1>
+            <p class="portfolio_site">eewano Portfolio Site</p>
         </div>
+        <p class="copyright">&copy;2020 eewano portfolio</p>
     </footer>
 
-    
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
