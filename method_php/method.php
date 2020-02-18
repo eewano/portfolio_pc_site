daf7f839ffb643bea59a8ef9d03b27868dbb1d07
eewano <eewano@gmail.com>
2020-02-18 12:11:49 +0900

    評価のレコードを数値に置き換えた為、評価数を星マークに変換する関数を追加。また、値段の出力関数名を変更。

--- method_php/method.php
+++ method_php/method.php
@@ -4,9 +4,29 @@ function h01($string) {
     return htmlspecialchars($string, ENT_QUOTES, "UTF-8");
 }
 
-function h02($string) {
+function h_price($string) {
     $yen = htmlspecialchars($string, ENT_QUOTES, "UTF-8");
     return number_format($yen);
+}
+
+function h_evaluation($val) {
+    if ($val > 7 || $val < 1) {
+        return '評価エラー';
+    }
+
+    $string = '';
+    $count = 0;
+
+    for ($i = 0; $i < $val; $i++) {
+        $string .= '★';
+        $count++;
+    }
+
+    for ($j = 0; $j < 7 - $val; $j++) {
+        $string .= '☆';
+    }
+
+    return htmlspecialchars($string, ENT_QUOTES, "UTF-8");
 }
 
 function sanitize($before) {
