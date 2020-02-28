edc7f2c34f46af602712d1fb9819b9b9cbd52695
eewano <eewano@gmail.com>
2020-02-28 22:34:03 +0900

    カート確認画面及び数量変更・削除プログラムphpの土台が完成。

---
+++ customer/customer_cart_change.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('../method_php/method.php');
+require_once('../method_php/get_user_pass.php');
+
+session_start();
+session_regenerate_id(true);
+
+$post = sanitize($_POST);
+
+$cart_max = $post['cart_max'];
+
+print '$cart_max = ' . $cart_max;
+print '<br>';
+
+for ($i = 0; $i < $cart_max; $i++) {
+    $quantity[] = $post['quantity' . $i];
+}
+
+$cart = $_SESSION['cart'];
+
+for ($i = $cart_max; 0 <= $i; $i--) {
+    if ($quantity[$i] == 0) {
+        array_splice($cart, $i, 1);
+        array_splice($quantity, $i, 1);
+    }
+}
+
+$_SESSION['cart'] = $cart;
+$_SESSION['quantity'] = $quantity;
+
+header('Location: customer_cart_look.php');
+exit();
+
+?>
