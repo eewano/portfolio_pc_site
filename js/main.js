f3a13d8be9d15bdaa60612dd969eb75209d89547
eewano <eewano@gmail.com>
2020-03-04 19:36:45 +0900

    画面横幅が狭いと隠れたメニューがはみ出してしまう不具合があった為、トグルボタンでメニューの表示・非表示を行なう処理を実装。

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ function myfunc() {
     let changeImg = document.querySelector(".img_change");
 
     if (mymenu.style.maxHeight) {
+        mymenu.classList.remove('visible');
         mymenu.style.maxHeight = null;
         changeImg.setAttribute('src', 'img/menu_icon_open.png');
     } else {
+        mymenu.classList.add('visible');
         mymenu.style.maxHeight = mymenu.scrollHeight + "px";
         changeImg.setAttribute('src', 'img/menu_icon_close.png');
     }
@@ -16,9 +18,11 @@ function myfunc_admin() {
     let changeImg = document.querySelector(".img_change");
 
     if (mymenu.style.maxHeight) {
+        mymenu.classList.remove('visible');
         mymenu.style.maxHeight = null;
         changeImg.setAttribute('src', '../img/menu_icon_open.png');
     } else {
+        mymenu.classList.add('visible');
         mymenu.style.maxHeight = mymenu.scrollHeight + "px";
         changeImg.setAttribute('src', '../img/menu_icon_close.png');
     }
